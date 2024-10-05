7b3b3ac67610a6c38a2ed968f8fc09b577447c3f
Daniel Dias <ecodelearn@outlook.com>
2024-10-05 11:42:45 -0300

    novo commit

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 """This module demonstrates a simple 'Hello, World!' example in Python."""
 
+
 print("Olá mundo Python!")
 
-variavel = 123
-
-OUTRA_VARIAVEL = 'valor'
-
-outra_variavel = 546
-
-MINHA_CONSTANTE = 123
+input() # O input() é usado para capturar o input do usuário
+print() # O print() é usado para exibir o resultado
