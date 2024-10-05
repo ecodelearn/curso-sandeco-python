554d466ba4160770906c249b6b9bb3be8b5703eb
Daniel Dias <ecodelearn@outlook.com>
2024-10-05 11:22:45 -0300

    ok

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+"""This module demonstrates a simple 'Hello, World!' example in Python."""
+
 print("Olá mundo Python!")
+
+variavel = 123
+
+OUTRA_VARIAVEL = 'valor'
+
+outra_variavel = 546
+
+MINHA_CONSTANTE = 123
